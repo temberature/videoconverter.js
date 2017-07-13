82cdb0ab8d16069f11ef15d11b0e5afbcba5fe6a
disoul <starky_ds@sina.cn>
2017-07-13 11:18:38 +0800

    fix ffmpeg complie error in emcc and support wasm

--- build/build_lgpl.sh
+++ build/build_lgpl.sh
@@ -17,7 +17,7 @@ cd ffmpeg
 #--enable-small
 
 make clean
-emconfigure ./configure --cc="emcc" --prefix=$(pwd)/../dist --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic \
+CPPFLAGS="-D_POSIX_C_SOURCE=200112 -D_XOPEN_SOURCE=600" emconfigure ./configure --cc="emcc" --prefix=$(pwd)/../dist --enable-cross-compile --target-os=none --arch=x86_64 --cpu=generic \
     --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network \
     --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-protocols --disable-indevs --disable-outdevs --enable-protocol=file \
 
@@ -31,10 +31,10 @@ cd dist
 
 rm *.bc
 
-cp lib/libz.a dist/libz.bc
+cp lib/libz.a libz.bc
 cp ../ffmpeg/ffmpeg ffmpeg.bc
 
-emcc -s OUTLINING_LIMIT=100000 -s VERBOSE=1 -s TOTAL_MEMORY=33554432 -O2 -v ffmpeg.bc -o ../ffmpeg.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js
+emcc -s WASM=1 -s VERBOSE=1 -s TOTAL_MEMORY=33554432 -O2 -v ffmpeg.bc -o ../ffmpeg.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js
 
 cd ..
 
